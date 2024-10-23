d992a0941d196c7a35d35ae71d1e5ad853fcedec
minhduongt <tnmnh1105@gmail.com>
2024-10-23 10:33:25 +0700

    fix: divide component link text

--- src/components/section/header/LinkText.jsx
+++ src/components/section/header/LinkText.jsx
@@ -1,5 +1,14 @@
-function LinkText({ label, toId }) {
-  return <p>{label}</p>;
+import { scrollToSectionById } from '@/utils';
+
+function LinkText({ label, id }) {
+  return (
+    <div
+      className="hover:bg-blue-primary cursor-pointer rounded-[30px] p-4 hover:text-white"
+      onClick={() => scrollToSectionById(id)}
+    >
+      <p className="font-gilroy text-small">{label}</p>
+    </div>
+  );
 }
 
 export default LinkText;
--- src/sections/header/NavigationBar.jsx
+++ src/sections/header/NavigationBar.jsx
@@ -1,6 +1,6 @@
 import Logo from '@/components/Logo';
+import LinkText from '@/components/section/header/LinkText';
 import { NAVIGATION_ITEMS } from '@/constants';
-import { scrollToSectionById } from '@/utils';
 
 function NavigationBar() {
   return (
@@ -9,13 +9,7 @@ function NavigationBar() {
       <div className="border-b-[1px] border-black py-1 pr-2">
         <div className="relative right-4 flex">
           {NAVIGATION_ITEMS.map((item) => (
-            <div
-              key={item.id}
-              className="hover:bg-blue-primary cursor-pointer rounded-[30px] p-4 hover:text-white"
-              onClick={() => scrollToSectionById(item.id)}
-            >
-              <p className="font-gilroy text-small">{item.label}</p>
-            </div>
+            <LinkText key={item.id} id={item.id} label={item.label} />
           ))}
         </div>
       </div>
