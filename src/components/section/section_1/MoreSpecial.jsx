157e03962910714fef86c7ec6a58e40e01b057b9
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-11-02 16:33:38 +0700

    feat: add section one MoreSpecial component

---
+++ src/components/section/section_1/MoreSpecial.jsx
@@ -0,0 +1,14 @@
+function MoreSpecial() {
+  return (
+    <div className="mt-5 max-w-[800px] rounded-lg bg-white p-4 text-left shadow-md">
+      <h2 className="text-lg font-bold text-red-600">Đặc biệt hơn...</h2>
+      <p className="mt-2">
+        Nhiều người từ chối phẫu thuật chủ yếu vì lo ngại biến chứng sau này,
+        như <span className="font-bold text-red-600">yếu cơ</span>,{' '}
+        <span className="font-bold text-red-600">tê liệt</span>.
+      </p>
+    </div>
+  );
+}
+
+export default MoreSpecial;
