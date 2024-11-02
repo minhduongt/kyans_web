b3f8fb7fafb443ab63c556bbe632f6909f6a83ad
Nguyen Vu Truong Huy <nguyenvutruonghuy112@gmail.com>
2024-11-02 16:33:07 +0700

    feat: improve section one

--- src/sections/section_1/index.jsx
+++ src/sections/section_1/index.jsx
@@ -1,5 +1,18 @@
+import MedicalCondition from '@components/section/section_1/MedicalCondition.jsx';
+import MoreSpecial from '@components/section/section_1/MoreSpecial.jsx';
+
 function SectionOne() {
-  return <div className="h-[1180px]">Thực trạng cột sống</div>;
+  return (
+    <section className="h-[1263px]">
+      {/* Group 127*/}
+      <div className="h-[920px]">
+        {/* Group 118 */}
+        <MedicalCondition />
+      </div>
+      {/* Group 125*/}
+      <MoreSpecial />
+    </section>
+  );
 }
 
 export default SectionOne;
